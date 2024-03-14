b253260774cdf907ea8cc209b0ca26fd329697b6
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-14 15:43:19 +0800

    membuat insertion sort

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -2,12 +2,19 @@
 
 void insertionsort() 
 {
-    int i, j, n;
+    int i, j, n, temp;
     int arr[20];
 
     for(i= 1; i <= n-1; i++)
     {
-       //step1
+       //step1 
+        temp = arr[i]; //step2
+
+        while (j >= 0 && arr[j] >= temp)//step4
+        {
+            arr[j + 1] = arr[j];//step4a
+            j = -1;//step4b
+        }
     }
 }
 
