77306eefc3c56db054955e5b6a1c0d38dab5ff17
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-14 16:15:29 +0800

    membuat display

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -29,6 +29,18 @@ void input()
 
 }
 
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+    }
+    cout << "jumlah pass = " << n - 1 << endl;
+    cout << endl;
+}
 
 void insertionsort() 
 {
