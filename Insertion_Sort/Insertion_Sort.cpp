8c3a238b31f69b6a6f4efac11750eeb57bf73c4a
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-14 16:13:52 +0800

    bikin input

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -1,9 +1,39 @@
-//#include <iostream>
+#include <iostream>
+using namespace std;
+
+int arr[40];
+int n;
+
+void input() 
+{
+    while (true)
+    {
+        cout << "masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
+}
+
 
 void insertionsort() 
 {
     int i, j, n, temp;
-    int arr[20];
+    int arr[40];
 
     for(i= 1; i <= n-1; i++)
     {
